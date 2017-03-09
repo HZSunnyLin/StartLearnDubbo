97f8a65f863bd277e5ec879e1184e1aa8fae9a26
hzsunnylin <hzsunnylin@163.com>
2017-03-09 22:45:42 +0800

    component

--- src/main/java/learndemo/dubboservice/dubboServiceAPIImpl.java
+++ src/main/java/learndemo/dubboservice/dubboServiceAPIImpl.java
@@ -1,12 +1,13 @@
 package learndemo.dubboservice;
 
 import DubboAPI.dubboServiceAPI;
+import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
 /**
  * Created by hzsunnylin on 2017/3/1.
  */
-@Service("dubboServiceAPI")
+@Component
 public class dubboServiceAPIImpl implements dubboServiceAPI {
     public String sayhellotoUser(String name){
         return new String("hello,"+name);
