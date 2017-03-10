e507e3979249e1a7b1d39a5531d87bf955133579
hzsunnylin <hzsunnylin@163.com>
2017-03-10 21:23:19 +0800

    dispatch log

--- src/main/java/learndemo/dubboservice/dubboServiceAPIImpl.java
+++ src/main/java/learndemo/dubboservice/dubboServiceAPIImpl.java
@@ -1,6 +1,8 @@
 package learndemo.dubboservice;
 
 import DubboAPI.dubboServiceAPI;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Component;
 import org.springframework.stereotype.Service;
 
@@ -9,6 +11,8 @@ import org.springframework.stereotype.Service;
  */
 @Component
 public class dubboServiceAPIImpl implements dubboServiceAPI {
+    Logger busilog= LoggerFactory.getLogger("businessLog");//只打业务日志
+    Logger execlog= LoggerFactory.getLogger(dubboServiceAPIImpl.class);//只打应用执行任务
     public String sayhellotoUser(String name){
         return new String("hello,"+name);
     }
